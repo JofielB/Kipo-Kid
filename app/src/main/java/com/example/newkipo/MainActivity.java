763e312dbd6fe0f48dd28cf991c868b41d786f7e
Jofiel Batista <jofielbatista@hotmail.com>
2020-04-13 22:13:14 -0600

    The displayed plant change when you click the triangle buttons
    In this commit i added this function but only works with an
    array, in the next fase i need to develope a plant class so in that
    way i will be able to do a lot of plants variations.

--- app/src/main/java/com/example/newkipo/MainActivity.java
+++ app/src/main/java/com/example/newkipo/MainActivity.java
@@ -7,11 +7,29 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnTriangleR, btnTriangleL;
+    private int index = 0;
+    private ImageView plant,pot;
+
+    private int pots[] = {
+            R.drawable.maceta_uno,
+            R.drawable.pot_2,
+            R.drawable.pot_3
+    };
+    private int plants[] = {
+            R.drawable.plant,
+            R.drawable.plant_2,
+            R.drawable.plant_3
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +38,36 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
 
+        plant = findViewById(R.id.imgPlant);
+        pot = findViewById(R.id.imageView7);
+
+        btnTriangleR = findViewById(R.id.btnTriangleRight);
+        btnTriangleL = findViewById(R.id.btnTriangleLeft);
+        btnTriangleR.setOnClickListener(this);
+        btnTriangleL.setOnClickListener(this);
+
         //HERE I NEED TO VERIFY IF THE USER HAD ANY PLANTS, DEPENDING OF THAT ONE ACTIVITY WILL BE LAUNCH OR ANOTHER
         /*Intent intent = new Intent(this,AddPlantActivity.class);
         startActivity(intent);
         finish();*/
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btnTriangleRight){
+            index++;
+            if(index>=3){
+                index = 0;
+            }
+        }else{
+            index--;
+            if(index<0){
+                index = 2;
+            }
+        }
+
+        plant.setImageResource(plants[index]);
+        pot.setImageResource(pots[index]);
+    }
+
 }
